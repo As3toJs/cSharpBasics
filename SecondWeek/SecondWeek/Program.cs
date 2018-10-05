b70984cd7cc7ff35e80495ec44c683309f06c1fe
piyush <piyush@edulab.dk>
2018-10-05 07:49:31 +0200

    initital commit

---
+++ SecondWeek/SecondWeek/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SecondWeek
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
